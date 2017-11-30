9ee7f42490bfd19446ab6009b35df1ffde5fc9fe
weichen <zalatmza@gmail.com>
2017-12-01 00:43:45 +0800

    loading

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -9,14 +9,18 @@ Page({
   },
   onLoad (option) {
     let _this = this
+    wx.showLoading({
+      mask: true,
+      title: 'loading'
+    })
     wx.request({
       url: `https://www.bilibili.com/read/cv${option.cvid}`,
       method: 'GET',
       dataType: 'text',
       success (res) {
         let html = res.data.match(/<div class="article-holder">.*<\/div>/)[0]
-        console.log(html)
         WxParse.wxParse('article', 'html', html, _this);
+        wx.hideLoading()
       }
     })
   }
